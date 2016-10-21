49e3a1dbd7504c580d18f7a868730ef7391d276a
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-21 16:47:25 -0400

    feat: added another component to other component
    another was added flat (in the same folder)

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { OtherComponent } from '.\other';
+import { OtherComponent, AnotherComponent } from './other';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  directives: ['OtherComponent']
+  directives: ['OtherComponent', 'AnotherComponent']
 })
 export class AppComponent {
   title = 'Hellow World!';
--- first-app/src/app/other/other.component.ts
+++ first-app/src/app/other/other.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+export { OtherComponent,AnotherComponent} from './other.component'
 @Component({
   selector: 'jenny-other',
   templateUrl: './other.component.html',
