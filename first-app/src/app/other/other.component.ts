f8d879fa6f6d1f6f766f6a6b9796a270cd2738d1
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-21 08:28:57 -0400

    feat: added new component and hooked up to the web-page
    jenny-other

---
+++ first-app/src/app/other/other.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jenny-other',
+  templateUrl: './other.component.html',
+  styleUrls: ['./other.component.css']
+})
+export class OtherComponent {
+ 	
+}
