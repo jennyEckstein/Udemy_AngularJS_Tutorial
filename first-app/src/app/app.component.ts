38b8a212017efebce86c90bc1d7972690deb5a88
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-21 08:45:34 -0400

    task: added directive to component decorator

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import {other} from '.\other';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: ['OtherComponent']
 })
 export class AppComponent {
   title = 'Hellow World!';
