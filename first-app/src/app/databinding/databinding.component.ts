da0b000361cfb35898d7e20a61fe81ffa46a0245
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-22 13:38:19 -0400

    feat: property binding froum outside

--- first-app/src/app/databinding/databinding.component.ts
+++ first-app/src/app/databinding/databinding.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { PropertyBindingComponent } from './property-binding.component.ts';
 
 @Component({
   selector: 'jenny-databinding',
   templateUrl: './databinding.component.html',
-  styleUrls: ['./databinding.component.css']
+  styleUrls: ['./databinding.component.css'],
+  directives: [PropertyBindingComponent]
 })
 export class DatabindingComponent {
 	stringInterpolation = "This is string interpolation"
 	numberInterpolation = 2;
 
 	onTest(){
-		return true;
+		return true; 
 	}
 }
