43110e6a7e6e08699d00a34cacf2cbbec984e247
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-27 09:11:33 -0400

    feat: added data service to first component

--- first-app/src/app/comp-a/comp-a.component.ts
+++ first-app/src/app/comp-a/comp-a.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
 import { LogService } from '../log.service';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-comp-a',
   templateUrl: './comp-a.component.html',
-  providers: [LogService]
+  providers: [LogService, DataService]
 })
 export class CompAComponent{
 	value = '';
-  constructor(private logService: LogService) {
+	items: string[] = [];
+  constructor(
+  	private logService: LogService,
+  	private dataService: DataService) {
   	
    }
 
    onLog(value:string){
    	this.logService.writeToLog(value);
+   }
+
+   onStore(value: string){
+   	this.dataService.addData(value);
+   }
+
+   onGet(value: string){
+   		this.items = this.dataService.getData();
    }
 
 }
---
+++ first-app/src/app/data.service.ts
@@ -0,0 +1,12 @@
+
+export class DataService {
+	private data:string[] = [];
+
+	addData(input:string){
+		this.data.push(input);
+	}
+
+	getData(){
+		return this.data;
+	}
+}
