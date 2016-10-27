23187ace7c4c8879842ba6a042ade4a874ced38c
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-27 09:00:36 -0400

    feat: added service to another location

--- first-app/src/app/comp-b/comp-b.component.ts
+++ first-app/src/app/comp-b/comp-b.component.ts
@@ -1,9 +1,16 @@
 import { Component} from '@angular/core';
+import { LogService } from '../log.service';
 
 @Component({
   selector: 'app-comp-b',
-  templateUrl: './comp-b.component.html'
+  templateUrl: './comp-b.component.html',
+  providers: [LogService]
 })
 export class CompBComponent{
+	value = '';
+	constructor(private logService: LogService){}
 
+	onLog(value:string){
+		this.logService.writeToLog(value);
+	}
 }
