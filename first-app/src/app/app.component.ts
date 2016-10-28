7eeb776bbb68191944aa1fe4a313447767af4028
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-28 16:30:31 -0400

    feat: routed /user to update component

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -2,29 +2,11 @@ import { Component } from '@angular/core';
 import { DatabindingComponent } from './databinding/databinding.component';
 import { LifecycleComponent } from './lifecycle.component';
 import { DirectivesComponent } from './directives/directives.component';
+import { routing } from './app.routes';
 
 @Component({
   selector: 'app-root',
-  template: `
-       <h3>Dependency Injection</h3>
-       <app-comp-a></app-comp-a>
-       <hr>
-       <app-comp-b></app-comp-b>
-       <hr>
-        
-  		<!--
-      <h1>Directives</h1>
-      <app-directives></app-directives>
-      <h1> Root Component </h1>
-  		<jenny-databinding></jenny-databinding>
-  		<jenny-lifecycle *ngIf="!delete" [bindable]="boundValue">
-  			<p>{{test}}</p>
-  		</jenny-lifecycle>
-  		<button (click)="delete = true"> Click to Delete </button>
-  		<button (click)="test = 'Changed Value'"> Click to Change </button>
-  		<button (click)="boundValue = '2000'"> Click to Change Binding </button>
-      -->
-  `
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
 	delete = false;
