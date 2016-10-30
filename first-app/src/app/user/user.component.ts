66fa6a3a1b8685950f8297eb73492db27b84d51f
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-30 12:08:17 -0400

    feat: fetching the id from the link

--- first-app/src/app/user/user.component.ts
+++ first-app/src/app/user/user.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
-import { Router } from "@angular/router";
+import { Component, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs/Rx";
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html'
 })
-export class UserComponent{
+export class UserComponent implements OnDestroy{
+	id: string;
+	private subscription: Subscription;
 
-	constructor(private router: Router){
+	constructor(private router: Router, 
+		private activatedRoute: ActivatedRoute){
+		/*does not update unless reload the component*/
+		/*this.id = activatedRoute.snapshot.params['id'];*/
 
+		/*may create memory leak unless onDestroy is implemented*/
+		this.subscription = activatedRoute.params.subscribe(
+				(param: any) => this.id = param['id']
+			);
 	}
 	onNavigate(){
 		this.router.navigate(['/']);
+	}
+
+	ngOnDestroy(){
+		this.subscription.unsubscribe();
 	}
  }
