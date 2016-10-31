e6cf85f382e193c2cf0c6ffaa4b58bb37811a6d1
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-31 08:45:12 -0400

    feat: extracting query parameters

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -3,13 +3,17 @@ import { DatabindingComponent } from './databinding/databinding.component';
 import { LifecycleComponent } from './lifecycle.component';
 import { DirectivesComponent } from './directives/directives.component';
 import { routing } from './app.routes';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent{
 	delete = false;
 	test = 'Starting Value';
 	boundValue = 1000;
+
+  
 }
--- first-app/src/app/client/client.component.ts
+++ first-app/src/app/client/client.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html'
 })
-export class ClientComponent implements OnInit {
+export class ClientComponent  implements OnDestroy{
+param: string;
+private subscription: Subscription;
 
-  constructor() { }
+ constructor(private activatedRoute: ActivatedRoute){
 
-  ngOnInit() {
-  }
+   this.subscription = activatedRoute.queryParams.subscribe(
+     (queryParam: any) => this.param = queryParam['analytics']
+     );
+ }
+
+ ngOnDestroy(){
+   this.subscription.unsubscribe();
+ }
 
 }
