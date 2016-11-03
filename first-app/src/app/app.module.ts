ab8b41728a51056a27ad2de71d8b8d3d54ce192b
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-11-03 18:09:27 -0400

    initial commit before learning forms

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,45 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { DatabindingComponent } from './databinding/databinding.component';
-import { PropertyBindingComponent } from './databinding/property-binding.component';
-import { EventBindingComponent } from './databinding/event-binding.component';
-import { TwoWayBindingComponent } from './databinding/two-way-binding.component';
-import { LifecycleComponent } from './lifecycle.component';
-import { DirectivesComponent } from './directives/directives.component';
-import { HighlightDirective } from './highlight.directive';
-import { UnlessDirective } from './unless.directive';
-import { CompAComponent } from './comp-a/comp-a.component';
-import { CompBComponent } from './comp-b/comp-b.component';
-import { DataService } from './data.service';
-import { LogService } from './log.service';
-import { UserComponent } from './user/user.component';
-import { routing } from './app.routes';
-import { ClientComponent } from './client/client.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    DatabindingComponent,
-    PropertyBindingComponent,
-    EventBindingComponent,
-    TwoWayBindingComponent,
-    LifecycleComponent,
-    DirectivesComponent,
-    HighlightDirective,
-    UnlessDirective,
-    CompAComponent,
-    CompBComponent,
-    UserComponent,
-    ClientComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    routing
+    HttpModule
   ],
-  providers: [DataService, LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- first-app/src/app/log.service.ts
+++
@@ -1,6 +0,0 @@
-
-export class LogService {
-	writeToLog(logMessage: string){
-		console.log(logMessage);
-	}
-}
