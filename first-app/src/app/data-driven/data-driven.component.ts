c47663cf7b100b0f6a56b14075bb454f577d1493
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-11-05 18:41:51 -0400

    feat: built data driven form

--- first-app/src/app/data-driven/data-driven.component.ts
+++ first-app/src/app/data-driven/data-driven.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven',
   templateUrl: './data-driven.component.html'
 })
-export class DataDrivenComponent implements OnInit {
+export class DataDrivenComponent implements OnInit{
 
-  constructor() { }
+	registerForm: FormGroup;
 
-  ngOnInit() {
-  }
+	constructor(private formBuilder: FormBuilder){}
 
+	ngOnInit(){
+		this.registerForm = this.formBuilder.group({
+			username: 'username',
+			email: 'email',
+			password: 'password'
+		});
+	}
+	
+	/*registerForm = new FormGroup({
+		username: new FormControl(),
+		email: new FormControl(),
+		password: new FormControl()
+	});*/
+
+
+ 
 }
