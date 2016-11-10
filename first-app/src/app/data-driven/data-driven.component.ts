2452c4bbde3a40ae2036ed194fd5ca32d9648a02
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-11-09 21:10:23 -0500

    feat: added radio buttons

--- first-app/src/app/data-driven/data-driven.component.ts
+++ first-app/src/app/data-driven/data-driven.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit} from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven',
   templateUrl: './data-driven.component.html'
 })
-export class DataDrivenComponent implements OnInit{
+export class DataDrivenComponent{
 
 	registerForm: FormGroup;	
+	sex = ['male', 'female'];
 
-	constructor(private formBuilder: FormBuilder){}
-
-	ngOnInit(){
+	constructor(private formBuilder: FormBuilder){
 		this.registerForm = this.formBuilder.group({
 			username: ['', [Validators.required, Validators.maxLength(10)]],
 			email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}')]],
-			password: ['', Validators.required]
+			password: ['', Validators.required],
+			gender: ""
 		});
 	}
+
 
 	onSubmit(){
 		console.log(this.registerForm);
