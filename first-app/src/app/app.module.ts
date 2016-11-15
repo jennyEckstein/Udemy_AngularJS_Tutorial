4072761c8fd092f425ae738d2875ea1fef0aaf27
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-11-14 20:55:46 -0500

    feat: coonected to firebase and logged data

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HttpService } from './http.service';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpModule
   ],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
