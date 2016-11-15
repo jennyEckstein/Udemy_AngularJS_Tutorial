965beaef980e987e882ee018aa8ddba10eb783a7
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-11-14 21:13:28 -0500

    feat: posted data to Firebase

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -11,8 +11,12 @@ export class AppComponent implements OnInit{
 
 	ngOnInit(){
 		this.httpService.getData().subscribe(
-				(data: Response) => console.log(data)
+				(data: any) => console.log(data)
 			);
+	}
+
+	onSubmit(username: string, email: string){
+		this.httpService.sendData({username: username, email: email}).subscribe(data => console.log(data));
 	}
 }
  
